56ffa0c39c125e7ac951d3ddd47c039f5a4a1e04
MBSoo <qud9783@khu.ac.kr>
2023-05-22 22:45:43 +0900

    commit

---
+++ files/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import File
+from .serializers import FileSerializer
+
+class FileList(generics.ListCreateAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+class FileDetail(generics.RetrieveAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
